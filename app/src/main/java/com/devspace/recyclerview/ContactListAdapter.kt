824738540fd6717254e3301820d161e427c9f259
GiovaneDev <giovane.santosdm@gmail.com>
2025-04-23 20:18:52 +0100

    Interagindo com RV na Activity

---
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -0,0 +1,48 @@
+package com.devspace.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactListAdapter :
+    ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list,parent,false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = getItem(position)
+        holder.bind(contact)
+    }
+
+    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val tvname = view.findViewById<TextView>(R.id.tv_name)
+        private val tvphone = view.findViewById<TextView>(R.id.tv_phone)
+        private val image = view.findViewById<ImageView>(R.id.Iv_image)
+
+        fun bind(contact: Contact) {
+            tvname.text = contact.name
+            tvphone.text = contact.name
+            image.setImageResource(contact.icon)
+        }
+
+    }
+
+    class ContactDiffUtils : DiffUtil.ItemCallback<Contact>() {
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem.name == newItem.name
+        }
+    }
+
+}
